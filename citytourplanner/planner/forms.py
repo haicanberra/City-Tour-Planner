700ad056d6d2861f96cfdbcd51db98096db575bb
Hai Hoang <haicanberra@gmail.com>
2023-06-01 02:02:18 -0600

    Implemented filters

--- citytourplanner/planner/forms.py
+++ citytourplanner/planner/forms.py
@@ -1,13 +1,95 @@
 from django import forms
 from .models import Search
 
+DOCUMENTATIONS = (
+    ('yes', 'Yes'),
+    ('no', 'No'),
+)
+
+# https://wiki.openstreetmap.org/wiki/Map_features#Tourism
+TOURISM_FILTERS = (
+    ('*','*'),
+    ('no', 'No'),
+    ('alpine_hut','Alpine hut'),
+    ('apartment','Apartment'),
+    ('aquarium','Aquarium'),
+    ('artwork','Artwork'),
+    ('attraction','Attraction'),
+    ('camp_pitch','Camp pitch'),
+    ('camp_site','Camp site'),
+    ('caravan_site','Caravan site'),
+    ('chalet','Chalet'),
+    ('gallery','Gallery'),
+    ('guest_house','Guest house'),
+    ('hostel','Hostel'),
+    ('hotel','Hotel'),
+    ('information','Information'),
+    ('motel','Motel'),
+    ('museum','Museum'),
+    ('picnic_site','Picnic site'),
+    ('theme_park','Theme park'),
+    ('viewpoint','Viewpoint'),
+    ('wilderness_hut','Wilderness hut'),
+    ('zoo','Zoo'),
+)
+
+# https://wiki.openstreetmap.org/wiki/Map_features#Amenity
+AMENITY_FILTERS = (
+    ('no', 'No'),
+    ('bar', 'Bar'),
+    ('biergarten','Biergarten'),
+    ('cafe','Cafe'),
+    ('fast_food','Fast food'),
+    ('food_court','Food court'),
+    ('ice_cream','Ice cream'),
+    ('pub','Pub'),
+    ('restaurant','Restaurant'),
+    ('library','Library'),
+    ('bicycle_rental','Bicycle rental'),
+    ('boat_rental','Boat rental'),
+    ('bus_station','Bus station'),
+    ('car_rental','Car rental'),
+    ('charging_station','Charging station'),
+    ('ferry_terminal','Ferry terminal'),
+    ('fuel','Fuel'),
+    ('parking','Parking'),
+    ('taxi','Taxi'),
+    ('atm','ATM'),
+    ('bank','Bank'),
+    ('hospital','Hospital'),
+    ('arts_centre','Arts centre'),
+    ('casino','Casino'),
+    ('cinema','Cinema'),
+    ('fountain','Fountain'),
+    ('nightclub','Nightclub'),
+    ('theatre','Theatre'),
+    ('townhall','Townhall'),
+    ('toilets','Toilets'),
+    ('marketplace','Marketplace'),
+)
+
 class SearchForm(forms.ModelForm):
     address = forms.CharField(
-        label = "Hotel's Address",
-        max_length = 100,
         required = True,
+        label = "City",
+        max_length = 100,
+    )
+    documented = forms.ChoiceField(
+        required = False,
+        label = "Documentations",
+        choices = DOCUMENTATIONS,
+    )
+    tourism_filters = forms.ChoiceField(
+        required = False,
+        label = "Tourism",
+        choices = TOURISM_FILTERS,
+    )
+    amenity_filters = forms.ChoiceField(
+        required = False,
+        label = "Amenity",
+        choices = AMENITY_FILTERS,
     )
 
     class Meta:
         model = Search
-        fields = ['address']
+        fields = ['address', 'documented', 'tourism_filters', 'amenity_filters']
--- citytourplanner/planner/views.py
+++ citytourplanner/planner/views.py
@@ -6,56 +6,87 @@ from django.http import HttpResponse
 
 from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
 from OSMPythonTools.nominatim import Nominatim
+from django.contrib import messages
 
 # Create your views here.
 def planner(request):
     nominatim = Nominatim()
     overpass = Overpass()
+    location = nominatim.query('Edmonton')
+    selector = ['"tourism"="attraction"']
+    documented = True
+    map = folium.Map(location=[53.631611, -113.323975], zoom_control=False)
 
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            address = form.cleaned_data.get("address")
+            location = nominatim.query(address)
+            lat, lon = location.toJSON()[0]['lat'], location.toJSON()[0]['lon']
+
+            amenity_filters = form.cleaned_data.get("amenity_filters")
+            tourism_filters = form.cleaned_data.get("tourism_filters")
+            if amenity_filters == "no":
+                if tourism_filters == '*':
+                    selector = ['"tourism"']
+                else:
+                    selector = ['"tourism"'+'"='+tourism_filters+'"']
+            else:
+                if tourism_filters == '*':
+                    selector = ['"tourism"','"amenity"']
+                else:
+                    selector = [
+                    '"tourism"='+'"'+tourism_filters+'"', 
+                    '"amenity"='+'"'+amenity_filters+'"'
+                    ]
+
+            documented = form.cleaned_data.get("documented")
+            query = overpassQueryBuilder(area=location,elementType=['node', 'way'],selector=selector,out='body')
+            result = overpass.query(query)
+
+            places_nodes = {}
+            places_ways = {}
+            tag_filters = ['wikidata', 'wikipedia', 'website', 'addr:city']
+            for i in result.elements():
+                if i.type() == "node":
+                    if i.lat() != None and i.lon() != None:
+                        if documented:
+                            for f in tag_filters:
+                                if i.tag(f)!=None and i.tag('name')!=None:
+                                    places_nodes[i.tag('name')] = (i.lat(), i.lon())
+                                    break
+                        else:
+                            if i.tag('name')!=None:
+                                    places_nodes[i.tag('name')] = (i.lat(), i.lon())
+                elif i.type() == "way":
+                    n = i.nodes()[0]
+                    if documented:
+                        for f in tag_filters:
+                            if i.tag(f)!=None and i.tag('name')!=None:
+                                places_ways[i.tag('name')] = (n.lat(), n.lon())
+                    else:
+                        if i.tag('name')!=None:
+                                places_ways[i.tag('name')] = (n.lat(), n.lon())
+            
+            # if lat==None or lng==None:
+            #     address.delete()
+            #     return HttpResponse('Invalid address?')
+            
+            map = folium.Map(location=[lat, lon], zoom_control=False)
+            for key in places_nodes:
+                folium.Marker(places_nodes[key], tooltip=key, popup=key).add_to(map)
+            for key in places_ways:
+                folium.Marker(places_ways[key], tooltip=key, popup=key).add_to(map)
+            if len(places_nodes) == 0 and len(places_ways) == 0:
+                messages.warning(request, "No places were found.")
+                return redirect('/')
     else:
         form = SearchForm()
-
-    address = Search.objects.all().last()
-    location = nominatim.query(address)
-    lat, lon = location.toJSON()[0]['lat'], location.toJSON()[0]['lon']
-    selector = ['"tourism"="attraction"']
-    query = overpassQueryBuilder(area=location,elementType=['node', 'way'],selector=selector,out='body')
-    result = overpass.query(query)
-
-    places_nodes = {}
-    places_ways = {}
-    tag_filters = ['wikidata', 'wikipedia', 'website', 'addr:city']
-    for i in result.elements():
-        if i.type() == "node":
-            if i.lat() != None and i.lon() != None:
-                for f in tag_filters:
-                    if i.tag(f)!=None and i.tag('name')!=None:
-                        places_nodes[i.tag('name')] = (i.lat(), i.lon())
-                        break
-        elif i.type() == "way":
-            n = i.nodes()[0]
-            for f in tag_filters:
-                if i.tag(f)!=None and i.tag('name')!=None:
-                    places_ways[i.tag('name')] = (n.lat(), n.lon())
-    
-    # if lat==None or lng==None:
-    #     address.delete()
-    #     return HttpResponse('Invalid address?')
-    
-    map = folium.Map(location=[lat, lon], zoom_control=False)
-    for key in places_nodes:
-        folium.Marker(places_nodes[key], tooltip=key, popup=key).add_to(map)
-    for key in places_ways:
-        folium.Marker(places_ways[key], tooltip=key, popup=key).add_to(map)
 
     map = map._repr_html_()
     context = {
         'map': map,
         'form': form,
-    }
+    } 
     return render(request, 'planner/planner.html', context)
